063bb1f10ff1c0693f5214d66696e91cd0e9e680
bart <bart.delrue@digipolis.gent>
2019-03-16 13:03:22 +0100

    Added museumpas fetch and reduce.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MuseumpasMatcherComponent } from './components/museumpas-matcher/museumpas-matcher.component';
+import { MuseumpasService } from './services/museumpas.service';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { MuseumpasMatcherComponent } from './components/museumpas-matcher/museum
     MuseumpasMatcherComponent
   ],
   imports: [
-    BrowserModule
+      HttpClientModule,
+      BrowserModule
   ],
-  providers: [],
+  providers: [MuseumpasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
