18195ff0cbd047ec299f0abf9e8feef256b917d9
Jefwillems <jefwillems100@gmail.com>
2019-03-16 11:28:55 +0100

    add tracker canvas component

---
+++ src/app/components/tracker-canvas/tracker-canvas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracker-canvas',
+  templateUrl: './tracker-canvas.component.html',
+  styleUrls: ['./tracker-canvas.component.scss']
+})
+export class TrackerCanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
