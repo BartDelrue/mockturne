acb2fd02254a49cad427332372f590e831909b22
Nick D'hondt <nick.dhondt@sweetmustard.be>
2019-03-16 14:27:56 +0100

    test

--- src/app/components/groeten/groeten.component.ts
+++ src/app/components/groeten/groeten.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-groeten',
   templateUrl: './groeten.component.html',
-  styleUrls: ['./groeten.component.scss']
+  styleUrls: ['./groeten.component.scss'],
 })
 export class GroetenComponent implements OnInit {
+  clicked = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
