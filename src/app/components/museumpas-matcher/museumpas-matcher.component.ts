c1057705d450103f0039ebcad8470575ed0414dd
Nick D'hondt <nick.dhondt@sweetmustard.be>
2019-03-16 13:55:23 +0100

    fuckn table

--- src/app/components/museumpas-matcher/museumpas-matcher.component.ts
+++ src/app/components/museumpas-matcher/museumpas-matcher.component.ts
@@ -3,26 +3,27 @@ import { MuseumpasService } from '../../services/museumpas.service';
 import { map } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-museumpas-matcher',
-    templateUrl: './museumpas-matcher.component.html',
-    styleUrls: ['./museumpas-matcher.component.scss']
+  selector: 'app-museumpas-matcher',
+  templateUrl: './museumpas-matcher.component.html',
+  styleUrls: ['./museumpas-matcher.component.scss'],
 })
 export class MuseumpasMatcherComponent implements OnInit {
+  constructor(private museumpasService: MuseumpasService) {}
 
-    constructor(private museumpasService: MuseumpasService) {
-    }
-
-    ngOnInit() {
-        this.museumpasService.onDataReady$.pipe(map(data => {
-            data = data.reduce((acc, current) => {
-                if (!acc[current.passholder_id_hackaton]) {
-                    acc[current.passholder_id_hackaton] = []
-                }
-                acc[current.passholder_id_hackaton].push(current);
-                return acc;
-            }, {});
-            return data
-        })).subscribe(data => console.table(data));
-    }
-
+  ngOnInit() {
+    this.museumpasService.onDataReady$
+      .pipe(
+        map(data => {
+          data = data.reduce((acc, current) => {
+            if (!acc[current.passholder_id_hackaton]) {
+              acc[current.passholder_id_hackaton] = [];
+            }
+            acc[current.passholder_id_hackaton].push(current);
+            return acc;
+          }, {});
+          return data;
+        }),
+      )
+      .subscribe();
+  }
 }
