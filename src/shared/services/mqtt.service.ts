2bfc6d5ee8ad622c2db342b1450be46cb681476e
Nick D'hondt <nick.dhondt@sweetmustard.be>
2019-03-16 12:04:16 +0100

    mqtt

--- src/shared/services/mqtt.service.ts
+++ src/shared/services/mqtt.service.ts
@@ -1,27 +1,41 @@
 import { Injectable } from '@angular/core';
-import { connect } from 'mqtt';
+import { fromEvent, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+declare var mqtt: any;
 
 @Injectable({
   providedIn: 'root',
 })
 export class MqttService {
   TOPIC = '5abca60f0e091b0005581409';
-  USERNAME = 'mockturne';
+  USERNAME = this.TOPIC;
   API_KEY = '030d3ab9-df02-4213-83de-90b31b86920a';
+  _client: any;
+  onMQTTMessage$: Observable<any>;
 
   constructor() {
-    const client = connect(
-      'wss://mqtt.cloud.pozyxlabs.com:443',
-      {
-        username: this.USERNAME,
-        password: this.API_KEY,
-      },
-    );
+    this._connect();
+    this.onMQTTMessage$ = this._MQTTMessage();
+    this.onMQTTMessage$.subscribe(message => console.log(message));
+  }
 
-    client.subscribe(this.TOPIC);
-    client.on('message', (topic, message) => {
-      console.log(message.toString());
-      console.log(topic);
+  private _connect() {
+    this._client = mqtt.connect('wss://mqtt.cloud.pozyxlabs.com:443', {
+      username: this.USERNAME,
+      password: this.API_KEY,
     });
+
+    this._client.subscribe(this.TOPIC);
+  }
+
+  private _MQTTMessage() {
+    return fromEvent<any>(this._client, 'message').pipe(
+      map(message => {
+        const data = JSON.parse(message[1].toString())[0];
+
+        return data;
+      }),
+    );
   }
 }
