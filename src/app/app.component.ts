2f9cc9374b69620a25654aeabe6db9cff0ce25e6
Wouter Vande Velde <wouter.vandevelde@student.odisee.be>
2019-03-16 14:31:01 +0100

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
+  public data: any;
   public mpData: any;
   public userData: {};
+  public matches: any;
 
   form: FormGroup;
 
   constructor(private museumpasService: MuseumpasService, private  fb: FormBuilder) {}
 
   ngOnInit() {
+    
     this.form = this.fb.group({
       mpas: [null, Validators.required]
     })
@@ -32,6 +34,44 @@ export class AppComponent implements OnInit {
       return data
     })).subscribe(data => {
       this.mpData = data
+      this.data = data;
+        console.log(data[4]);
+        
+        var mijnId:number = 4; 
+        var num:number = 1; 
+        var score:number = 0; 
+        var besteScore:number = 0;
+        var matchIDfirst = -1;
+        var matchIDsecond = -1;
+        var matchIDthird = -1;
+
+        this.matches.push()
+        let mijnMuseums: string[];
+        mijnMuseums = [];
+        for (let entry of data[mijnId]) {
+            mijnMuseums.push(entry.museumnaam_nl);
+        }
+
+        while(num<5000) {
+            score = 0;
+            if(data[num] != null){
+                for (let entry of data[num]) {
+                    if(mijnMuseums.indexOf(entry.museumnaam_nl)) {
+                        score++;
+                    }
+                    if(besteScore<score) {
+                        besteScore=score;
+                        var matchIDfirst = num;
+                    }
+                }
+                
+            }
+            
+            num++;
+        }
+
+        this.matches.push({id: matchIDfirst, code: '9000'});
+        console.log(matchIDfirst);
     });
   }
 
